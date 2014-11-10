22b63ed00138ba754a5e9f9c4c0ff0f69b41081b
n0tu <maxi2812@gmx.net>
2014-11-10 04:33:56 +0100

    CHAT
    ENABLED

--- s.js
+++ s.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(80);
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
 
-console.log('Running at :80');
+http.listen(80, function(){
+  console.log('listening on *:80');
+});
