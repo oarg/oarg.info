a2f46022a6969334f25bce1b70997f9aca38b7f0
n0tu <maxi2812@gmx.net>
2014-11-19 16:29:57 +0100

    hzfg
    gfgf

--- s.js
+++ s.js
@@ -11,6 +11,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(80, function(){
-  console.log('listening on *:80');
+http.listen(8080, function(){
+  console.log('listening on *:8080');
 });
